293747cb4f0fb790ce233ec7888eb202eaffb401
NotRealSilver <notrealsilver@yandex.com>
2026-02-10 18:11:36 +0500

    task 5 and 6

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from modules import *
 from PIL import Image
 
 WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 450
+WINDOW_HEIGHT = 500
 WINDOW_TITLE = "Часть 3"
 KEY_SET = {
     arcade.key.PAGEUP,
@@ -26,10 +26,11 @@ class MapView(arcade.Window):
         self.lon = 60.109599
         self.lat = 55.050432
         self.theme = 'light'
+        self.pt = f'{self.lon},{self.lat},comma'
 
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
-        self.layout = arcade.gui.UIBoxLayout(x=20, y=400, width=500, height=50, vertical=False)
+        self.layout = arcade.gui.UIBoxLayout(x=20, y=450, width=500, height=50, vertical=False)
 
         self.on_texture = arcade.load_texture(
             ":resources:gui_basic_assets/simple_checkbox/circle_on.png"
@@ -37,14 +38,30 @@ class MapView(arcade.Window):
         self.off_texture = arcade.load_texture(
             ":resources:gui_basic_assets/simple_checkbox/circle_off.png"
         )
-        toggle_label = arcade.gui.UILabel(text='Темная тема', text_color=arcade.color.GRAY, font_size=14, bold=True)
+        toggle_label = arcade.gui.UILabel(text='Темная тема', text_color=arcade.color.WHITE, font_size=14)
         toggle = arcade.gui.UITextureToggle(on_texture=self.on_texture, off_texture=self.off_texture, width=20,
                                             height=20)
         toggle.on_change = self.change_theme
 
+        search_label = arcade.gui.UILabel(text='Поиск', text_color=arcade.color.WHITE, font_size=14)
+        self.search_field = arcade.gui.UIInputText(width=100, height=22, text_color=arcade.color.WHITE)
+        search_btn = arcade.gui.UIFlatButton(text='Искать', width=55, height=20)
+        search_btn.on_click = self.on_search
+
         self.layout.add(toggle_label)
         self.layout.add(toggle)
+        self.layout.add(search_label)
+        self.layout.add(self.search_field)
+        self.layout.add(search_btn)
         self.manager.add(self.layout)
+        self.get_image()
+
+    def on_search(self, event):
+        text = self.search_field.text
+        if not text:
+            return
+        self.lon, self.lat = get_geoobject_coord(geocode(text))
+        self.pt = f'{self.lon},{self.lat},flag'
         self.get_image()
 
     def change_theme(self, val):
@@ -78,9 +95,11 @@ class MapView(arcade.Window):
         if symbol in KEY_SET:
             self.get_image()
 
-
     def get_image(self):
-        im = static_maps(ll=f'{self.lon},{self.lat}', z=self.zoom, theme=self.theme)
+        params = dict(ll=f'{self.lon},{self.lat}', z=self.zoom, theme=self.theme)
+        if self.pt:
+            params['pt'] = self.pt
+        im = static_maps(**params)
         imdata = arcade.texture.ImageData(Image.open(im).convert('RGBA'))
         self.background = arcade.Texture(imdata)
 
